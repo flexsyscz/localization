d389035d66042eebaf5a1f9b29fc4acfb9a7729a
Václav Lukeš <vasek@flexsys.cz>
2026-03-16 09:38:01 +0100

    update tests

--- tests/Resources/SupportedLanguages.php
+++ tests/Resources/SupportedLanguages.php
@@ -11,4 +11,28 @@ enum SupportedLanguages: string implements Flexsyscz\Localization\Translations\S
 	case Czech = 'cs_CZ';
 	case English = 'en_US';
 	case Slovak = 'sk_SK';
+
+
+	public function getShortCode(): string
+	{
+		$shortCodes = [
+			self::Czech->value => 'cs',
+			self::English->value => 'en',
+			self::Slovak->value => 'sk',
+		];
+
+		return $shortCodes[$this->value] ?? '';
+	}
+
+
+	public function getDescription(): string
+	{
+		$descriptions = [
+			self::Czech->value => 'Čeština',
+			self::English->value => 'English',
+			self::Slovak->value => 'Slovenština',
+		];
+
+		return $descriptions[$this->value] ?? '';
+	}
 }
